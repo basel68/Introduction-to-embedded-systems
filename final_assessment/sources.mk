7134dfad86f861bcc74f266a0c8c798bc88a34f8
basel68 <baselfarouk09@gmail.com>
2023-08-21 22:51:13 +0300

    addinf files to the final assessment

--- final_assessment/sources.mk
+++ final_assessment/sources.mk
@@ -14,19 +14,22 @@ ifeq ($(PLATFORM),MSP432)
 	SOURCES = *.c
 
 	# Add your include paths to this variable
-	INCLUDES = ../include/CMSIS/cmsis_gcc.h \
-	   	../include/CMSIS/core_cm4.h \
-	   	../include/CMSIS/core_cmFunc.h \
-	   	../include/CMSIS/core_cmInstr.h \
-	   	../include/CMSIS/core_cmSimd.h \
-	   	../include/common/memory.h \
-	   	../include/common/platform.h \
-	   	../include/msp432/msp432p401r.h \
-	   	../include/msp432/msp_compatibility.h \
-		../include/msp432/system_msp432p401r.h
+	INCLUDES = ./include/CMSIS/cmsis_gcc.h \
+	   	./include/CMSIS/core_cm4.h \
+	   	./include/CMSIS/core_cmFunc.h \
+	   	./include/CMSIS/core_cmInstr.h \
+	   	./include/CMSIS/core_cmSimd.h \
+	   	./include/common/memory.h \
+	   	./include/common/platform.h \
+		./include/common/stats.h \
+	   	./include/msp432/msp432p401r.h \
+	   	./include/msp432/msp_compatibility.h \
+		./include/msp432/system_msp432p401r.h
 else
-	SOURCES = main.c  memory.c
+	SOURCES = main.c  memory.c stats.c
 
-	INCLUDES =../include/common/memory.h \
-		../include/common/platform.h
+	INCLUDES =./include/common/memory.h \
+	   	./include/common/platform.h \
+		./include/common/stats.h 
+
 endif
