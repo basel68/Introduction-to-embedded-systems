fe781406ff9db170a5f38362fae285cf13a347a8
basel68 <baselfarouk09@gmail.com>
2023-08-23 20:19:48 +0300

    m

--- final_assessment/main.c
+++ final_assessment/main.c
@@ -23,10 +23,10 @@
  */
 
 #include "memory.h"
-#include "stats.h"
-#include "data.h"
-#include "course1.h"
-#include "platform.h"
+#include stats.h
+#include data.h
+#include course1.h
+#include platform.h
 #define MAX_LENGTH (10)
 
 char buffer[MAX_LENGTH];
